ad33498260408e44d03d18dd50a7869e096549ff
olivier-desmet77 <119798266+olivier-desmet77@users.noreply.github.com>
2023-03-23 07:01:31 +0100

    Autheur fait - non corrigé

--- src/Controller/AdminAuthorController.php
+++ src/Controller/AdminAuthorController.php
@@ -2,17 +2,94 @@
 
 namespace App\Controller;
 
+use App\Form\AuthorType;
+use App\Repository\AuthorRepository;
+use DateTime;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class AdminAuthorController extends AbstractController
 {
-    #[Route('/admin/author', name: 'app_admin_author')]
-    public function index(): Response
+
+    ////// Methode permettant de créer un nouvel auteur
+    #[Route('/admin/auteur/nouveau', name: 'app_admin_author_create')]
+    public function create(Request $request, AuthorRepository $repository): Response
     {
-        return $this->render('admin_author/index.html.twig', [
-            'controller_name' => 'AdminAuthorController',
+        // Création du formulaire
+        $form = $this -> createForm(AuthorType::class);
+        // Remplir le formulaire avec les donnée de saisie
+        $form->handleRequest($request);
+        // Test si le formulaire envoyé et validé
+        if ($form -> isSubmitted() && $form -> isValid() ){
+            // Récupèration de l'auteur dans le formulaire
+            $Author = $form
+                ->getData()
+                ->setCreatedAt(new DateTime())
+                ->setUpdateAt(new DateTime());
+        // Enregistrer l'auteur dans la base de donnée
+        $repository->save($Author, true);
+        // Rediriger vers la liste des autheurs
+        return $this->redirectToRoute('app_admin_author_list');
+        
+        }
+    }
+    //Afficher la page de creation d'un livre
+    return $this->render('admin_author/create.html.twig', [
+        'controller_name' => 'AdminAuthorController',
+    ]);
+
+    ////// Liste de tous les autheurs
+    #[Route('/admin/auteur', name: 'app_admin_author_list')]
+    public function list(AuthorRepository $repository): Response
+    {
+        // Récupérer tous les auteurs de la base de donnée
+        $Author = $repository -> findAll();
+        // Afficher la liste de autheurs
+        return $this -> render('admin_author/list.html.twig',[
+            // Envoyer les auteurs à twig
+            'authors' => $Authors,
         ]);
     }
+
+    ///// Mise à jour des autheurs
+    #[Route('/admin/auteurs/{id}', name: 'app_admin_author_update')]
+    public function update(Author $author, Request $request, AuthorRepository $repository): Response
+    {
+       // Création du formulaire
+       $form = $this -> createForm(AuthorType::class);
+       // Remplir le formulaire avec les donnée de saisie
+       $form->handleRequest($request);
+       // Test si le formulaire envoyé et validé
+        if ($form -> isSubmitted() && $form -> isValid()) {
+        // Enregistrer l'autheur dans la base de données
+         $repository -> save($author -> setUpdateAt(new DateTime()), true);
+         // Rediriger vers la liste des livres
+        return $this->redirectToRoute('app-admin_author_list');
+       } 
+    // Afficher la page de mise à jour
+    return $this->render('admin_author/update.html.twig',[
+        'form' => createView(),
+    ]);
+    }
+
+    ///// Supprimer un autheur de la BDD
+    #[Route('/admin/auteurs/{id}/supprimer', name: 'app_admin_author_remove')]
+    public function remove(Author $author, AuthorRepository $repository): Response
+    {
+        $repository-> remove($author,true);
+        // Rediriger vers la liste des autheurs
+        return $this->redirectToRoute('app_admin_author_list');
+
+    }
+
+
+
+
+
+
+
+
+
 }
--- src/Form/AuthorType.php
+++ src/Form/AuthorType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\Author;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,18 +13,27 @@ class AuthorType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('title')
-            ->add('description')
-            ->add('nationality')
-            ->add('createAt')
-            ->add('updatedAt')
-        ;
+            ->add('title', TextType::class,[
+                'label' => "Nom de l'auteur",
+            ])
+            ->add('description', TextareaType::class,[
+                'label' => 'Description',
+            ])
+            ->add('nationality', TextType::class,[
+                'label' => 'Nationnalite',
+            ])
+            // ->add('createAt')
+            // ->add('updatedAt')
+            ->add('submit', SubmitType::class,[
+                'label' => 'Envoyer'
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
             'data_class' => Author::class,
+            'method' => 'POST'
         ]);
     }
 }
